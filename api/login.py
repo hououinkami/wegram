b3397e1862c135feb6267bf984d05dc653446f0e
hououinkami <hououinkami@gmail.com>
2025-05-22 17:48:48 +0800

    新增初始化函数

--- api/login.py
+++ api/login.py
@@ -4,6 +4,12 @@ import config
 from typing import Dict, Any
 from api.base import wechat_api
 
+def heartbeat(wxid):
+    api_path="/Login/HeartBeat"
+    query={"wxid": wxid}
+    response = wechat_api(api_path=api_path, query_params=query)
+    return response
+
 def get_profile(wxid):
     api_path="/User/GetContractProfile"
     query={"wxid": wxid}
@@ -16,11 +22,34 @@ def twice_login(wxid):
     response = wechat_api(api_path=api_path, body=body)
     return response
 
-def newinit(wxid, body: Dict[str, Any] = None):
+def newinit(wxid, max_synckey = "", current_synckey = ""):
     api_path="/Login/Newinit"
-    query={"wxid": wxid}
-    response = wechat_api(api_path=api_path, body = body, query_params=query)
-    return response
+    # query={"wxid": wxid}
+    body = {
+        "wxid": wxid,
+        "MaxSynckey": max_synckey,
+        "CurrentSynckey": current_synckey
+    }
+    response = wechat_api(api_path=api_path, body = body)
+
+    if response.status != 200:
+        return None
+    
+    # 检查内容类型
+    content_type = response.headers.get('Content-Type', '')
+    if 'application/json' not in content_type and 'text/json' not in content_type:
+        # 尝试读取文本内容以便调试
+        text = response.text()
+        return None
+    
+    # 解析JSON响应
+    json_resp = response.json()
+
+    if json_resp.get("Success"):
+        return json_resp.get("Data")
+    
+    else:
+        return None
 
 def get_cached_info(wxid):
     api_path="/Login/GetCacheInfo"
